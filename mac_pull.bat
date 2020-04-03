ad4f7df0a22b47bcf3260df6e5be0af74516aaf1
skducn <skducn@163.com>
2020-04-03 13:10:02 +0800

    from mac

--- mac_pull.bat
+++ mac_pull.bat
@@ -1,13 +1,25 @@
-del cms.mdb
-git pull
+del .\db\cms.mdb
+
+
+git pull origin master
+
+
 
 c:
+
 cd\
+
 ipconfig /all >ip.txt
+
+
 for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (
 httpcfg set iplisten -i %%b
 
+
+
 del ip.txt
+
+
 "C:\Users\JohnWork\AppData\Local\Google\Chrome\Application\chrome.exe" %%b
 )
 )
