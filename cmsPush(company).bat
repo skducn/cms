6933af4efda191e67023658c9c193f38ea110613
skducn <skducn@163.com>
2020-07-21 17:46:07 +0800

    DESKTOP-EOCO1V0

--- cmsPush(company).bat
+++ cmsPush(company).bat
@@ -2,6 +2,7 @@ git add .
 
 set name = %date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 
-git commit -m %date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %computername%
+%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 git push origin HEAD:master
 
