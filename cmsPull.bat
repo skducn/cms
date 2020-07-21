07d7a52bcceb8f32e5a05acafef61cdc9715b657
skducn <skducn@163.com>
2020-07-21 18:10:32 +0800

    WIN764_20200721181032

--- cmsPull.bat
+++ cmsPull.bat
@@ -1,14 +1,26 @@
 
 git stash
+
+
 git pull origin master
 
+
+
 c:
+
 cd\
+
 ipconfig /all >ip.txt
+
+
 for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (
 httpcfg set iplisten -i %%b
 
+
+
 del ip.txt
+
+
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %%b
 )
 )
--- cmsPush.bat
+++ cmsPush.bat
@@ -1,7 +1,12 @@
 @echo off
+
+
 git add .
 
+
+
 git commit -m %computername%_%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+
 
 git push origin HEAD:master
 
