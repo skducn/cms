99087df7f63f0dfcff7b128bd68311167a16632a
skducn <skducn@163.com>
2020-07-22 09:24:47 +0800

    WIN764_202007229:2447

--- cmsPullByMac.bat
+++ cmsPullByMac.bat
@@ -1,15 +1,23 @@
 
 git stash
 
+
+
 git pull origin master
+
+
 
 
 
 c:
 
+
 cd\
 
+
 ipconfig /all >ip.txt
+
+
 
 
 for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (
@@ -17,7 +25,11 @@ httpcfg set iplisten -i %%b
 
 
 
+
+
 del ip.txt
+
+
 
 
 "C:\Users\JohnWork\AppData\Local\Google\Chrome\Application\chrome.exe" %%b
