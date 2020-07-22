ebb0267a8b375924d851a8dbee1ed0a088072e34
skducn <skducn@163.com>
2020-07-22 09:15:26 +0800

    DESKTOP-EOCO1V0_202007229:1525

--- cmsPush.bat
+++ cmsPush.bat
@@ -1,14 +1,16 @@
-@echo off
-
+@echo on
 
 git add .
 
 
 
-git commit -m %computername%_%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m %computername%_%date:~0,4%%date:~5,2%%date:~8,2%%time:~1,2%%time:~3,2%%time:~6,2%
 
 
 
 git push origin master
 
 
+
+
+pause
