d6ee70502ad0e0d312d87230ca84f0c04491f2d8
skducn <skducn@163.com>
2020-10-15 17:38:14 +0800

    LHCWIN7_20201015_173814

--- 1cmsPullByWin764.bat
+++ 1cmsPullByWin764.bat
@@ -1,7 +1,8 @@
 @echo off
 
 
-
+git stash
+git pull origin master
 c:
 
 
@@ -10,7 +11,6 @@ cd\
 
 ipconfig /all >ip.txt
 for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (httpcfg set iplisten -i %%b
-
 del ip.txt
 "C:\Program Files\Google\Chrome\Application\chrome.exe" %%b
 ))
