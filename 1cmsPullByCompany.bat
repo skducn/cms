ed319ae42ce1709f756ff5d7e5baed14b5d16998
skducn <skducn@163.com>
2021-01-15 17:17:30 +0800

    DESKTOP-DO7AEPU_20210115_7:1730

--- 1cmsPullByCompany.bat
+++ 1cmsPullByCompany.bat
@@ -14,8 +14,7 @@ for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (
 httpcfg set iplisten -i %%b
 
 
-
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %%b
+"C:\Program Files\Google\Chrome\Application\chrome.exe" %%b
 )
 )
 
